57708490abb826bf85b403fa31e28a230e15b717
ryo2357 <miyawakir@hosokawa-yoko.co.jp>
2023-01-06 13:57:15 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Greet from "./components/Greet.vue";
+</script>
+
+<template>
+  <div class="container">
+    <h1>Tauri機能検証</h1>
+
+    <h2>Vue ⇒ Rust の invoke</h2>
+    <Greet />
+
+
+  </div>
+</template>
+
+<style scoped>
+</style>
